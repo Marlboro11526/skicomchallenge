1ab538dbe95124238f32a9e25a68ad6dc042ffea
Marlboro11526 <RichardJohnston33@outlook.com>
2021-01-23 02:34:17 -0800

    ADD: init basic react routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,57 @@
-import './App.css';
-import Form from './Form';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import Form from './Form'
+
+export default function App() {
   return (
-    <div className="App">
-      <Form></Form>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/registration">Add User</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/registration">
+            <Form />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function AddUser() {
+  return <h2>Add User</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
