c9fdc662a097d76c1524e2214108f2c695ca1039
Marlboro11526 <RichardJohnston33@outlook.com>
2021-01-23 01:41:54 -0800

    ADD: Cors configurations

--- api/src/main.rs
+++ api/src/main.rs
@@ -15,6 +15,31 @@ mod db;
 pub mod resorts;
 pub mod users;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, _request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[catch(500)]
 fn internal_error() -> &'static str {
     "We've encountered an unexpected error."
@@ -23,6 +48,7 @@ fn internal_error() -> &'static str {
 fn main() {
     dotenv().ok();
     rocket::ignite()
+        .attach(CORS())
         .register(catchers![internal_error])
         .manage(db::init_pool())
         .mount(
