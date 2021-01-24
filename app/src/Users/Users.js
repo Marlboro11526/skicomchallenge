614b7830e9f7b06ebcbc13a87c5dda01e54b031e
Marlboro11526 <RichardJohnston33@outlook.com>
2021-01-23 20:22:36 -0800

    ADD: sortable table

--- app/src/Users/Users.js
+++ app/src/Users/Users.js
@@ -3,71 +3,62 @@ import { useEffect, useState } from 'react';
 
 function Users() {
     const [users, setUsers] = useState(null);
-    const [frequencies, setFrequencies] = useState(null);
+    const [sortedBy, setSortedBy] = useState("");
 
     const fetchData = async () => {
-      if(!users){
-        await fetch("http://localhost:8000/users").then(res => res.json()).then(async _users => {
-          setUsers(_users)
-          let frequencyTable = {};
-          await _users.forEach(user => {
-            return frequencyTable[user.favorite_resort] = isNaN(frequencyTable[user.favorite_resort]) ? 0 : frequencyTable[user.favorite_resort] + 1;
-          });
-          setFrequencies(frequencyTable);
-          console.log(frequencies);
-
-        }).catch(e => console.log(e));
-
-        
-      }
+        await fetch("http://localhost:8000/users")
+        .then(res => res.json())
+        .then(data => setUsers(data))
+        .catch(console.log);
   }
 
-
-
   const sortUsers = type => {
+    let sortedUsers = [];
     switch(type){
-      case "name": break;
-      case "email": break;
-      case "resort": break;
+      case "first":
+        sortedUsers = [...users.sort((a,b) => a.first_name > b.first_name ? 1 : -1)]; 
+        break;
+      case "last":
+        sortedUsers = [...users.sort((a,b) => a.last_name > b.last_name ? 1 : -1)]; 
+        break;
+      case "email": 
+        sortedUsers = [...users.sort((a,b) => a.email > b.email ? 1 : -1)]; 
+        break;
+      case "resort": 
+        sortedUsers = [...users.sort((a,b) => a.favorite_resort > b.favorite_resort ? 1 : -1)]; 
+        break;
       default: break;
     }
+    setSortedBy(type);
+    setUsers(sortedUsers); 
+
   }
 
   useEffect(() => {
     fetchData();
-  })
+  }, []);
 
   return (
-<>
 <table className="table-auto">
   <thead>
     <tr>
-      <th onClick={sortUsers("name")}>Full Name</th>
-      <th onClick={sortUsers("email")}>Email Address</th>
-      <th onClick={sortUsers("resort")}>Favorite Resort</th>
+      <th onClick={() => sortUsers("first")}>First Name {sortedBy === "first" ? "▼" : ""}</th>
+      <th onClick={() => sortUsers("last")}>Last Name {sortedBy === "last" ? "▼" : ""}</th>
+
+      <th onClick={() => sortUsers("email")}>Email Address {sortedBy === "email" ? "▼" : ""}</th>
+      <th onClick={() => sortUsers("resort")}>Favorite Resort {sortedBy === "resort" ? "▼" : ""}</th>
     </tr>
   </thead>
   <tbody>
-    {users && users.map(item => <tr key={item._id.$oid}>
-    <td>{`${item.first_name} ${item.last_name}`}</td>
+  {users && users.map(item => <tr key={item._id.$oid}>
+    <td>{item.first_name}</td>
+    <td>{item.last_name}</td>
     <td>{item.email}</td>
     <td>{item.favorite_resort}</td>
   </tr>)}
+    
   </tbody>
 </table>
-
-<table className="table-auto">
-  <thead>
-    <tr>
-      <th>Resort</th>
-      <th>Registered Users</th>
-    </tr>
-  </thead>
-  <tbody>
-
-  </tbody>
-</table>
-</>
   );
 }
 
