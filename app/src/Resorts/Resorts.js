b76c527d2d41ecbd5fffeb1e761e9f60910fcbb0
Marlboro11526 <RichardJohnston33@outlook.com>
2021-01-24 03:49:18 -0800

    ADD: resort update request and controls

--- app/src/Resorts/Resorts.js
+++ app/src/Resorts/Resorts.js
@@ -2,22 +2,43 @@ import { useEffect, useState } from 'react';
 
 function Users() {
     const [resorts, setResorts] = useState(null);
-    const [sortedBy, setSortedBy] = useState("");
-    const [frequencies, setFrequencies] = useState({});
+    const [editable, setEditable] = useState("");
+    const [editedText, setEditedText] = useState("");
+
 
     const fetchData = async () => {
         await fetch("http://localhost:8000/resorts")
         .then(res => res.json())
         .then(data => setResorts(data))
         .catch(console.log);
+
+        setEditable("");
+        setEditedText(""); 
   }
 
   const addResort = async (resort) => {
     await fetch("http://localhost:8000/resorts");
 }
 
-const updateResort = async (resort) => {
-    await fetch("http://localhost:8000/resorts");
+const updateResort = async (id) => {
+
+    if(!editedText){
+        setEditable("");
+        return;
+    }
+
+    await fetch(`http://localhost:8000/resorts/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: editedText })
+    }).then(
+    () => {
+        fetchData();
+    }
+    );
+
 }
 
   const deleteResort = async (resortId) => {
@@ -31,22 +52,47 @@ const updateResort = async (resort) => {
     fetchData();
   }
 
-  const enableEdit = (e) => {
+  const enableEdit = e => {
       e.target.contentEditable = true;
+      e.target.defaultValue = "";
+      e.target.value = "";
+
+  }
+
+  const onInputChange = e => {
+      setEditedText(e.target.value);
   }
 
   useEffect(() => {
     fetchData();
   }, []);
 
+
+
   return (
 <>
 <table className="table-auto">
+    <thead>
+        <tr>
+            <th>Resort Name</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
   <tbody>
   {resorts && resorts.map(item => <tr key={item._id.$oid}>
-    <td><input  type="text" value={item.name}/></td>
-    <td onClick={enableEdit}>🖉</td>
-    <td onClick={() => deleteResort(item._id.$oid)}>✖</td>
+    { editable !== item._id.$oid ? 
+        <>
+            <td>{item.name}</td>
+            <td onClick={() => setEditable(item._id.$oid)}>🖉</td>
+        </>
+        : 
+        <>
+            <td><input onChange={onInputChange} type="text"/></td>
+            <td onClick={() => updateResort(item._id.$oid)}>{editedText ? "🖫" : "✖"}</td>
+        </>
+        }
+    <td onClick={() => deleteResort(item._id.$oid)}>🗑</td>
   </tr>)}
     
   </tbody>
